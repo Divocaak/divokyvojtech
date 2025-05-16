79393badbee091f8ca9094d3fa7f879a9ae520ae
Vojtěch divoký <divokyvojta@gmail.com>
2025-05-16 11:18:00 +0200

    gulas, homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<svelte:head>
+    <title>DIVOKÝ VOJTĚCH</title>
+</svelte:head>
+
+<h1>vojtěch divoký</h1>
+<!-- <a href="/prices">ceník</a><br> -->
+<a href="/gulas">recept na guláš</a><br>
+<br>
+<a target="_blank" href="https://instagram.com/divokyvojta/">instagram</a><br>
+<a target="_blank" href="https://facebook.com/divokyv/">facebook</a><br>
+<a target="_blank" href="https://twitter.com/DivokyVojtech">twitter</a><br>
+<a target="_blank" href="https://github.com/Divocaak">github</a><br>
+<a target="_blank" href="https://linkedin.com/in/vojtechdivoky">linkedin</a><br>
+<a target="_blank" href="mailto:divokyvojta@gmail.com">psát mail</a><br>
+<a target="_blank" href="https://www.rzp.cz/cgi-bin/aps_cacheWEB.sh?VSS_SERV=ZVWSBJFND&OKRES=&CASTOBCE=&OBEC=&ULICE=&CDOM=&COR=&COZ=&ICO=&OBCHJM=&OBCHJMATD=0&ROLES=P&JMENO=Vojt%ECch&PRIJMENI=Divok%FD&NAROZENI=&ROLE=&VYPIS=1&type=&PODLE=osoba&Action=List&IDOSOBY=52e17bde18efe09685ce">info v rejstříku</a><br>
