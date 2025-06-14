bd142e3bcefbdd981fd497f8e4f89614c8bf8832
Vojtěch divoký <divokyvojta@gmail.com>
2025-06-14 22:57:06 +0200

    daksi content

--- src/routes/gulas/+page.svelte
+++ src/routes/gulas/+page.svelte
@@ -2,12 +2,25 @@
 	<title>RECEPT NA GULÁŠ</title>
 </svelte:head>
 <a href="/">z5</a>
+
+<h2>další přísady</h2>
+<p>hej já už fakt nevim co sem psát vole :Dd</p>
+<img src="/gulas/meeting.jpg" alt="mail conversation screenshot" />
+<img src="/gulas/procenta.jpg" alt="mail conversation screenshot" />
+<p>tohle je ale největší skvost, tajná ingredience, nebo tak</p>
+<video width="320" height="240" controls>
+	<source src="/gulas/video.mp4" type="video/mp4" />
+	<track kind="captions" />
+	Your browser does not support the video tag.
+</video>
+
 <h2>koření</h2>
 <p>dochutíme podle chutí, co má kdo rád</p>
 <img src="/gulas/maily_kveten/mail.jpg" alt="mail conversation screenshot" />
 <img src="/gulas/maily_kveten/mail2.jpg" alt="mail conversation screenshot" />
 
-<h2>beers & screams</h2>
+<h2>základ na omáčku: pivo a křik</h2>
+<p>haha, ta akce se jmenovala "beers & screams," chápeš ne</p>
 <p>
 	<b>lore:</b>výhrot kvůli pozdě založenej události a mému <i>pasivnímu až ignorantskému</i> zájmu o
 	akci jako takovou. mailová komunikace ale tvrdí opak, všechno probíhalo v intervalech cca tří dnů,
@@ -26,7 +39,7 @@
 <img src="/gulas/bs/1.jpg" alt="messenger screenshot" />
 <img src="/gulas/bs/2.jpg" alt="messenger screenshot" />
 
-<h2>jenom tak do rezervy</h2>
+<h2>volitelné ingredience</h2>
 <p>takový další hezký zprávy, který bolí</p>
 <img src="/gulas/0.jpg" alt="messenger screenshot" />
 <img src="/gulas/1.jpg" alt="messenger screenshot" />
@@ -36,10 +49,18 @@
 <img src="/gulas/5.jpg" alt="messenger screenshot" />
 <img src="/gulas/6.jpg" alt="messenger screenshot" />
 
-<p>až budu mít někdy víc času, tak prolezu všechny konverzace za poslední dva roky a dám sem fakt úplně všechno. protože je to dva roky prakticky úplně stejný</p>
+<h2>servírujeme s křupavým chlebem</h2>
+<p>nebo jiným pečivem</p>
+<img src="/gulas/vyhra.jpg" alt="messenger screenshot" />
+
+<p>
+	až budu mít někdy víc času, tak prolezu všechny konverzace za poslední dva roky a dám sem fakt
+	úplně všechno. protože je to dva roky prakticky úplně stejný
+</p>
 
 <style>
-	img {
+	img,
+	video {
 		margin: 0;
 		padding: 0;
 		position: relative;
@@ -48,19 +69,20 @@
 		max-height: 50%;
 	}
 
-    h2{
-        border-top: 2px solid crimson;
-        padding-top: 2rem;
-    }
+	h2 {
+		border-top: 2px solid crimson;
+		padding-top: 2rem;
+	}
 
-    h2:last-of-type, p:last-of-type{
-        margin-top: 10rem;
-    }
+	h2:last-of-type,
+	p:last-of-type {
+		margin-top: 10rem;
+	}
 
 	@media (max-width: 1200px) {
 		img {
 			width: 100%;
-            max-height: none;
+			max-height: none;
 		}
 	}
 </style>
