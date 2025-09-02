8c9b6a6c9a47398f2aeb8655516f051f77499af3
Vojtěch divoký <divokyvojta@gmail.com>
2025-09-02 11:28:25 +0200

    homepage about me, projects, etc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,350 @@
-
 <svelte:head>
-    <title>DIVOKÝ VOJTĚCH</title>
+	<title>DIVOKÝ VOJTĚCH</title>
+	<script type="application/ld+json">
+		[
+			{
+				"@context": "https://schema.org",
+				"@type": "Person",
+				"@id": "https://divokyvojtech.cz/",
+				"name": "Vojtěch Divoký",
+				"alternateName": "Divoký Vojtěch",
+				"url": "https://divokyvojtech.cz/",
+				"description": "Vojtěch Divoký — programátor, zvukař, osvětlovač a videotechnik",
+				"email": "mailto:divokyvojta@gmail.com",
+				"sameAs": [
+					"https://instagram.com/divokyvojta/",
+					"https://facebook.com/divokyv/",
+					"https://twitter.com/DivokyVojtech",
+					"https://github.com/Divocaak",
+					"https://linkedin.com/in/vojtechdivoky"
+				],
+				"knowsAbout": [
+					"Web development",
+					"App development",
+					"SvelteKit",
+					"Kultura",
+					"Hudební festivaly",
+					"Videoprodukce",
+					"Divadlo",
+					"Zvuk",
+					"Světlo",
+					"Video"
+				],
+				"worksFor": [
+					{
+						"@type": "Organization",
+						"name": "Žižkárna",
+						"url": "https://zizkarna.cz/"
+					},
+					{
+						"@type": "Organization",
+						"name": "DHOGH Festival",
+						"url": "https://dhogh.com/"
+					}
+				],
+				"subjectOf": [
+					{
+						"@type": "NewsArticle",
+						"headline": "Budějovický deník o Žižkárně",
+						"url": "https://ceskobudejovicky.denik.cz/zpravy_region/budejovicky-hudebni-klub-zizkarna-koupil-nove-podium-od-celniku-20230617.html"
+					},
+					{
+						"@type": "NewsArticle",
+						"headline": "Budějovický deník o areálu Žižkových kasáren",
+						"url": "https://ceskobudejovicky.denik.cz/zpravy_region/zizova-kasarna-ceske-budejovice.html"
+					},
+					{
+						"@type": "NewsArticle",
+						"headline": "Deník N - Postcovidový syndrom hudební scény",
+						"url": "https://denikn.cz/1297229/postcovidovy-syndrom-hudebni-sceny-ceske-hudebni-kluby-bojuji-o-preziti-z-krize-je-muzete-vyvest-jen-vy/"
+					},
+					{
+						"@type": "Article",
+						"headline": "Pořadatelská Fullmoon o DHOGH festivalu",
+						"url": "https://www.fullmoonzine.cz/poradatelska-vojtech-divoky-dhogh-festival"
+					},
+					{
+						"@type": "Article",
+						"headline": "Czechmag - WHO* IS o DHOGHu",
+						"url": "https://www.instagram.com/p/DKjd0kzsz-3/"
+					}
+				]
+			},
+
+			{
+				"@context": "https://schema.org",
+				"@type": "CreativeWork",
+				"name": "HOW, IF AT ALL",
+				"url": "https://youtu.be/NgLYB06i-LE",
+				"creator": {
+					"@type": "Person",
+					"@id": "https://divokyvojtech.cz/"
+				}
+			},
+			{
+				"@context": "https://schema.org",
+				"@type": "CreativeWork",
+				"name": "I'LL GO EASY",
+				"url": "https://youtu.be/Ptg-BdYegKY",
+				"creator": {
+					"@type": "Person",
+					"@id": "https://divokyvojtech.cz/"
+				}
+			},
+			{
+				"@context": "https://schema.org",
+				"@type": "WebSite",
+				"name": "Žižkárenský program",
+				"url": "https://program.zizkarna.cz/",
+				"author": {
+					"@type": "Person",
+					"@id": "https://divokyvojtech.cz/"
+				}
+			},
+			{
+				"@context": "https://schema.org",
+				"@type": "WebSite",
+				"name": "DIY Caravan Festival",
+				"url": "https://camper.zizkarna.cz/",
+				"author": {
+					"@type": "Person",
+					"@id": "https://divokyvojtech.cz/"
+				}
+			},
+			{
+				"@context": "https://schema.org",
+				"@type": "WebSite",
+				"name": "Galerie Nahoře",
+				"url": "https://galerienahore.cz/",
+				"author": {
+					"@type": "Person",
+					"@id": "https://divokyvojtech.cz/"
+				}
+			},
+			{
+				"@context": "https://schema.org",
+				"@type": "WebSite",
+				"name": "UPSALA s.r.o.",
+				"url": "https://upsala.cz/",
+				"author": {
+					"@type": "Person",
+					"@id": "https://divokyvojtech.cz/"
+				}
+			},
+			{
+				"@context": "https://schema.org",
+				"@type": "WebSite",
+				"name": "DHOGH Festival",
+				"url": "https://dhogh.com/",
+				"author": {
+					"@type": "Person",
+					"@id": "https://divokyvojtech.cz/"
+				}
+			},
+			{
+				"@context": "https://schema.org",
+				"@type": "WebSite",
+				"name": "butwedoit",
+				"url": "https://butwedoit.cz/",
+				"author": {
+					"@type": "Person",
+					"@id": "https://divokyvojtech.cz/"
+				}
+			},
+			{
+				"@context": "https://schema.org",
+				"@type": "WebSite",
+				"name": "EuropeFixer",
+				"url": "https://europefixer.com/",
+				"author": {
+					"@type": "Person",
+					"@id": "https://divokyvojtech.cz/"
+				}
+			},
+			{
+				"@context": "https://schema.org",
+				"@type": "WebSite",
+				"name": "Atlas hub",
+				"url": "https://atlashub.supsck.cz/",
+				"author": {
+					"@type": "Person",
+					"@id": "https://divokyvojtech.cz/"
+				}
+			},
+			{
+				"@context": "https://schema.org",
+				"@type": "WebSite",
+				"name": "Kreativní platforma",
+				"url": "https://www.kreatins.cz/kreativni-platforma/",
+				"author": {
+					"@type": "Person",
+					"@id": "https://divokyvojtech.cz/"
+				}
+			},
+			{
+				"@context": "https://schema.org",
+				"@type": "WebSite",
+				"name": "kuchynevancata",
+				"url": "https://kuchynevancata.cz/",
+				"author": {
+					"@type": "Person",
+					"@id": "https://divokyvojtech.cz/"
+				}
+			},
+			{
+				"@context": "https://schema.org",
+				"@type": "WebSite",
+				"name": "openwatercze",
+				"url": "https://openwatercze.cz/",
+				"author": {
+					"@type": "Person",
+					"@id": "https://divokyvojtech.cz/"
+				}
+			}
+		]
+	</script>
 </svelte:head>
 
 <h1>vojtěch divoký</h1>
-<!-- <a href="/prices">ceník</a><br> -->
-<a href="/gulas">recept na guláš</a><br>
-<br>
-<a target="_blank" href="https://instagram.com/divokyvojta/">instagram</a><br>
-<a target="_blank" href="https://facebook.com/divokyv/">facebook</a><br>
-<a target="_blank" href="https://twitter.com/DivokyVojtech">twitter</a><br>
-<a target="_blank" href="https://github.com/Divocaak">github</a><br>
-<a target="_blank" href="https://linkedin.com/in/vojtechdivoky">linkedin</a><br>
-<a target="_blank" href="mailto:divokyvojta@gmail.com">psát mail</a><br>
-<a target="_blank" href="https://www.rzp.cz/cgi-bin/aps_cacheWEB.sh?VSS_SERV=ZVWSBJFND&OKRES=&CASTOBCE=&OBEC=&ULICE=&CDOM=&COR=&COZ=&ICO=&OBCHJM=&OBCHJMATD=0&ROLES=P&JMENO=Vojt%ECch&PRIJMENI=Divok%FD&NAROZENI=&ROLE=&VYPIS=1&type=&PODLE=osoba&Action=List&IDOSOBY=52e17bde18efe09685ce">info v rejstříku</a><br>
+<a href="/gulas">recept na guláš</a><br />
+<h2>kontakty</h2>
+<a target="_blank" href="https://instagram.com/divokyvojta/">instagram</a><br />
+<a target="_blank" href="https://facebook.com/divokyv/">facebook</a><br />
+<a target="_blank" href="https://twitter.com/DivokyVojtech">twitter</a><br />
+<a target="_blank" href="https://github.com/Divocaak">github</a><br />
+<a target="_blank" href="https://linkedin.com/in/vojtechdivoky">linkedin</a><br />
+<br />
+<a target="_blank" href="mailto:divokyvojta@gmail.com">psát mail</a><br />
+<h2>věci</h2>
+<p>
+	v roce 23 jsem nějak trochu omylem svítil 2 klipy pro Tolkyho, tak si je sem odložím
+	<a href="https://youtu.be/NgLYB06i-LE?si=0DtYUdw82sczKXrm">HOW, IF AT ALL</a> a
+	<a href="https://youtu.be/Ptg-BdYegKY?si=UQ-A-QqcjH_1lKR9">I'LL GO EASY</a><br />
+	<br />
+	jeden čas jsem dělal projekz
+	<a target="_blank" href="https://www.youtube.com/@Zizkarna/playlists">žžTV</a>, záznamy z
+	koncertů, ještě toho mám hromadu v šuplíku a jednou bych to chtěl dodělat
+</p>
+<h2>weby, webappky</h2>
+<p>
+	<a target="_blank" href="https://fermannovaspirala.cz/login">WebFerman Nová Spirála</a><br />
+	<br />
+	<a target="_blank" href="https://upsala.cz/">UPSALA s.r.o.</a>, web má plnohodnotný admin
+	rozhraní, pevně napsaná stránka je snad jenom <i>Studio</i>, jinak je na každé stránce něco
+	generováno skrz admin.<br />
+	<br />
+	<a target="_blank" href="https://dhogh.com/">DHOGH (festival)</a>, srdcovka, pokemoní kartičky
+	jako lístky<br />
+	<br />
+	<b>Žižkárna</b>, krom <a target="_blank" href="https://zizkarna.cz/">hlavního webu</a> si roku
+	2025 zasloužil svůj web i
+	<a target="_blank" href="https://camper.zizkarna.cz/">DIY Caravan Festival</a>.<br />
+	Nekonečným, x let trvajícím projektem je
+	<a target="_blank" href="https://program.zizkarna.cz/">žižkárenský program</a>. Neslouží totiž jen
+	jako program, pod povrchem se skrývá databáze kapel, generátor videí (reels/stories) jako
+	měsíční/týdenní přehledů akcí, generátor textací pro FB události (pro zachování struktury u každé
+	akce), automatický export do Guidle (služba 3. strany spravující (mimo jiné) oficiální městský
+	kalendář akcí) a různé analytické a statistické nástroje (kolik nových kapel u nás hrálo v roce x,
+	jaký žánr byl tenhle měsíc nejhranější, apod.). Časem je plánovaný update s automatizací celého
+	backofficu, který provoz klubu potřebuje.<br />
+	<br />
+	web <a target="_blank" href="https://galerienahore.cz/">Galerie Nahoře</a> až se doplní contentem,
+	tak bude fakt krásnej<br />
+	<br />
+	<a target="_blank" href="https://butwedoit.cz/">butwedoit</a> a jejich sesterská služba
+	<a target="_blank" href="https://europefixer.com/">europefixer</a> (a dalších x domén)<br />
+	<br />
+	projekt <a target="_blank" href="https://atlashub.supsck.cz/">Atlas hub</a> pro SUPŠ sv. Anežky
+	České v Českém Krumlově<br />
+	<br />
+	další x let trvající projekt je
+	<a target="_blank" href="https://www.kreatins.cz/kreativni-platforma/">jeden z modulů</a>
+	pro Kreativní Institut<br />
+	<br />
+	<a target="_blank" href="https://kuchynevancata.cz/">kuchynevancata</a> a
+	<a target="_blank" href="https://openwatercze.cz/">openwatercze</a>, hrozně rychlý a fakt nehezký
+	weby, ale požadadavky jsou požadavky :D
+</p>
+<h2>(bohužel) mrtvé/pozastavené projekty</h2>
+<h3>čučfest</h3>
+<p>
+	už asi mrtvý <a target="_blank" href="https://cucfest.cz/">web</a> charitativního festivalu
+	alternativní hudby, stihli jsme udělat dva ročníky a pomoct tam, kde to bylo potřeba, třeba se k
+	tomu zase někdy vrátíme.
+	<a target="_blank" href="https://www.instagram.com/cucfest/">instagram</a> a
+	<a target="_blank" href="https://www.facebook.com/cucfest">facebook</a> ale žijou dál, s moc hezkýma
+	fotkama
+</p>
+<h3>pivovar garáž</h3>
+<p>
+	taky pravděpodobně mrtvý <a target="_blank" href="https://pivovargaraz.cz/">web</a> Pivovaru
+	Garáž, web pro nás slloužil i jako databáze vařených várek, množství, rezervační systém pro
+	zákazníky, žije už jenom
+	<a target="_blank" href="https://www.facebook.com/garaz.pivovar">instagram</a> a
+	<a target="_blank" href="https://www.instagram.com/pivovar.garaz/">facebook</a>
+</p>
+<h2>občas někde mediálně</h2>
+<a
+	target="_blank"
+	href="https://ceskobudejovicky.denik.cz/zpravy_region/budejovicky-hudebni-klub-zizkarna-koupil-nove-podium-od-celniku-20230617.html"
+>
+	Budějovický deník o Žižkárně
+</a><br />
+<a
+	target="_blank"
+	href="https://ceskobudejovicky.denik.cz/zpravy_region/zizova-kasarna-ceske-budejovice.html"
+>
+	Budějovický deník, obecně o areálu Žižkových kasáren
+</a><br />
+<a
+	target="_blank"
+	href="https://denikn.cz/1297229/postcovidovy-syndrom-hudebni-sceny-ceske-hudebni-kluby-bojuji-o-preziti-z-krize-je-muzete-vyvest-jen-vy/?ref=tit?ref=mwat"
+>
+	Deník N - Postcovidový syndrom hudební scény
+</a><br />
+<a target="_blank" href="https://www.fullmoonzine.cz/poradatelska-vojtech-divoky-dhogh-festival">
+	Pořadatelská Fullmoon o DHOGH festivalu
+</a><br />
+<a target="_blank" href="https://www.instagram.com/p/DKjd0kzsz-3/?img_index=1">
+	czechmagovský WHO* IS, taky o DHOGHu
+</a>
+<h2>jamu projekty</h2>
+<a
+	target="_blank"
+	href="http://difanet.jamu.cz/archivy/covid_studenti/pracovni/monodramata-2024/Rekl-Che20241202.mp4"
+>
+	Řekl Che (A)
+</a><br />
+<a
+	target="_blank"
+	href="http://difanet.jamu.cz/archivy/covid_studenti/pracovni/monodramata-2024/R%cc%8cekl-Che-II20241210.mp4"
+>
+	Řekl Che (B)
+</a><br />
+<a
+	target="_blank"
+	href="http://difanet.jamu.cz/archivy/covid_studenti/pracovni/monodramata-2024/Lovu-zdar20241216.mp4"
+>
+	Lovu zdar! (A)
+</a><br />
+<a
+	target="_blank"
+	href="http://difanet.jamu.cz/archivy/covid_studenti/pracovni/monodramata-2024/Lovu-zdar20241217.mp4"
+>
+	Lovu zdar! (B)
+</a><br />
+<a
+	target="_blank"
+	href="http://difanet.jamu.cz/archivy/covid_studenti/pracovni/bakal-proj-2025/Zrusit-rodinu20250226.mp4"
+>
+	Zrušit rodinu (A)
+</a><br />
+<a
+	target="_blank"
+	href="http://difanet.jamu.cz/archivy/covid_studenti/pracovni/bakal-proj-2025/Zrusit-rodinu20250423.mp4"
+>
+	Zrušit rodinu (B)
+</a><br />
+<a target="_blank" href="https://www.youtube.com/watch?v=svthqesRPPk">
+	Zrušit rodinu (tech záznam)
+</a><br />
