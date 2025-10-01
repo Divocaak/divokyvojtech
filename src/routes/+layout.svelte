c17daead8916ad53fdf11b2b84070c0598bce2fe
Vojtěch divoký <divokyvojta@gmail.com>
2025-10-01 14:23:03 +0200

    rfs imgs, vystavovani

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
     }
 
     :global(a){
-        color: crimson
+        color: crimson;
     }
 
     :global(p){
--- src/routes/rfs/+page.svelte
+++ src/routes/rfs/+page.svelte
@@ -5,3 +5,56 @@
 <p>pak tu taky časem bude odkaz na to video viďžejo</p>
 <p>a taky blatomirovo grafika</p>
 <p>asi by bylo dobrý zmínit i rfs z roku 2024</p>
+<p>
+	fotečky z předpremiéry a premiéry od <a
+		href="https://www.instagram.com/tntjukebox/"
+		target="_blank">Pavel Balek</a
+	>:
+</p>
+
+<div class="img-wrapper">
+    <img src="/rfs/5.jpg" alt="race for space 1.1" />
+	<img src="/rfs/0.jpg" alt="race for space 1.1" />
+    <img src="/rfs/6.jpg" alt="race for space 1.1" />
+	
+	<img src="/rfs/10.jpg" alt="race for space 1.1" />
+	<img src="/rfs/16.jpg" alt="race for space 1.1" />
+    <img src="/rfs/7.jpg" alt="race for space 1.1" />
+	
+    <img src="/rfs/11.jpg" alt="race for space 1.1" />
+    <img src="/rfs/1.jpg" alt="race for space 1.1" />
+	<img src="/rfs/12.jpg" alt="race for space 1.1" />
+	
+    <img src="/rfs/13.jpg" alt="race for space 1.1" />
+    <div class="landscape">
+        <img src="/rfs/14.jpg" alt="race for space 1.1" />
+        <img src="/rfs/18.jpg" alt="race for space 1.1" />
+    </div>
+	<img src="/rfs/15.jpg" alt="race for space 1.1" />
+    
+    <img src="/rfs/2.jpg" alt="race for space 1.1" />
+    <div class="landscape">
+        <img src="/rfs/4.jpg" alt="race for space 1.1" />
+        <img src="/rfs/3.jpg" alt="race for space 1.1" />
+    </div>
+	<img src="/rfs/9.jpg" alt="race for space 1.1" />
+</div>
+
+<style>
+    .img-wrapper{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: .25rem;
+    }
+
+    .landscape{
+        display: flex;
+        flex-direction: column;
+        gap: .25rem;
+        justify-content: space-evenly;
+    }
+
+    .img-wrapper img{
+        width: 100%;
+    }
+</style>
